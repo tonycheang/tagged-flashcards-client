2128445c012c779a90c0026514cf6e8f82273104
Tony Cheang <cheangytony@gmail.com>
2019-06-16 19:09:05 -0700

    Updates

---
+++ precache-manifest.a20b9c110e87ec6724d19f4f9e579251.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d183bb0b065153b405c68e409966df52",
+    "url": "/japanese-flash-cards/index.html"
+  },
+  {
+    "revision": "d4b4316097847d5fec19",
+    "url": "/japanese-flash-cards/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "2790c44532533aa1f119",
+    "url": "/japanese-flash-cards/static/js/2.2668f2aa.chunk.js"
+  },
+  {
+    "revision": "d4b4316097847d5fec19",
+    "url": "/japanese-flash-cards/static/js/main.c4e25bbf.chunk.js"
+  },
+  {
+    "revision": "bdce2934d99eea959ece",
+    "url": "/japanese-flash-cards/static/js/runtime~main.e143d4ba.js"
+  }
+]);
