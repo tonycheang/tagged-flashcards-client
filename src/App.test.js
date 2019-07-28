7f1fae2a3e56ec7425ac459792c9f730123c55dc
Tony Cheang <cheangytony@gmail.com>
2019-07-28 15:58:29 -0700

    removed generic named files

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import FlashCardApp from './App';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<FlashCardApp />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
