ae98bfe5369553787bae957d6a8059351d0cc3aa
Tony Cheang <cheangytony@gmail.com>
2019-08-30 23:06:54 -0700

    added tests for deck operation and deck operation side effect validation

--- src/Site.test.js
+++ src/Site.test.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import { shallow, render, mount } from 'enzyme';
 import Site from './Site';
-import { Deck, FlashCard } from './Deck';
+import { Deck, FlashCard, buildDefaultDeck } from './Deck';
 import ManageDeckPage from './ManageDeckPage';
 import TransferTagsModal from './TransferTagsModal';
-import FlashCardApp from './FlashCardApp'
+import FlashCardApp from './FlashCardApp';
 import { Menu, message } from 'antd';
-import { Button } from 'antd/lib/radio';
 
 // Work around for JSDdom issue with jest mocks (August '18):
 // https://github.com/facebook/jest/issues/6798
@@ -16,9 +15,10 @@ const clearSpy = jest.spyOn(Storage.prototype, "clear");
 
 beforeEach(() => {
     localStorage.clear();
-    setItemSpy.mockClear();
-    getItemSpy.mockClear();
-    clearSpy.mockClear();
+});
+
+afterEach(() => {
+    jest.clearAllMocks();
 });
 
 it("renders without crashing", () => {
@@ -258,28 +258,199 @@ describe("using the navbar", () => {
 });
 
 it("changes the active card upon method call", () => {
-    throw Error("Test not yet implemented.");
+    spyOn(Deck.prototype, "getNextCard");
+    const siteWrap = shallow(<Site />);
+    const startingCard = siteWrap.state().currentCard;
+    expect(Deck.prototype.getNextCard).toHaveBeenCalledTimes(1);
+    siteWrap.instance().changeCard();
+    expect(Deck.prototype.getNextCard).toHaveBeenCalledTimes(2);
+    siteWrap.unmount();
 });
 
 describe("deck operations", () => {
-    it("change the deck", () => {
-        throw Error("Test not yet implemented.");
+
+    let siteWrap, deckOps;
+
+    beforeEach(() => {
+        const deck = new Deck();
+        const cards = [
+            new FlashCard("foo", "bar", "", ["baz"]),
+            new FlashCard("water", "juice", "", ["drinks"]),
+            new FlashCard("orange", "apple", "", ["fruits"]),
+            new FlashCard("jackfruit", "tomato", "", ["fruits"]),
+            new FlashCard("dog", "cat", "", ["animals"]),
+            new FlashCard("lion", "bear", "", ["animals"]),
+            new FlashCard("jackalope", "herring", "", ["animals"])
+        ]
+        cards.forEach((card) => { deck.appendCard(card) });
+        siteWrap = shallow(<Site />);
+        // Ensure we use the fake deck
+        siteWrap.setState({ deck, currentCard: deck.getNextCard() });
+        deckOps = siteWrap.instance().deckOps;
     });
 
-    it("serialize the deck", () => {
-        throw Error("Test not yet implemented.");
+    afterEach(() => {
+        siteWrap.unmount();
     });
 
-    it("rebuild the deck", () => {
-        throw Error("Test not yet implemented.");
+    it("can append a card to the deck", () => {
+        const additionalCard = new FlashCard("added", "new", "card", ["tag1", "tag2"])
+
+        deckOps.appendCard(additionalCard);
+        const deckContainsAdditionalCard = deckOps.getListOfCards().some((card) => card === additionalCard);
+
+        expect(deckContainsAdditionalCard).toBe(true);
     });
 
-    it("refresh the active card", () => {
-        throw Error("Test not yet implemented.");
+    it("can edit a card in the deck", () => {
+        const DECK_KEY = 2;
+        const originalCard = deckOps.getCardFromKey(DECK_KEY);
+        const fakeValues = { front: "edited", back: "this", prompt: "card", tags: ["tag1", "tag2"] };
+
+        deckOps.editCard(DECK_KEY, fakeValues);
+
+        const card = deckOps.getCardFromKey(DECK_KEY);
+        // edited card present
+        expect(card).toEqual(expect.objectContaining(fakeValues));
+
+        // old card gone
+        deckOps.getListOfCards().forEach((card) => {
+            expect(card).not.toEqual(expect.objectContaining(originalCard));
+        });
+    });
+
+    it("can delete a card from the deck", () => {
+        const DECK_KEY = 2;
+        const originalCard = deckOps.getCardFromKey(DECK_KEY);
+
+        deckOps.deleteCard(DECK_KEY);
+
+        deckOps.getListOfCards().forEach((card) => {
+            expect(card).not.toEqual(expect.objectContaining(originalCard));
+        });
+    });
+
+    it("can delete multiple cards from the deck", () => {
+        const DECK_KEYS = [3, 4, 5];
+        const originalCards = DECK_KEYS.map(key => deckOps.getCardFromKey(key));
+
+        deckOps.deleteCards(DECK_KEYS);
+
+        deckOps.getListOfCards().forEach((card) => {
+            originalCards.forEach((deletedCard) => {
+                expect(card).not.toEqual(expect.objectContaining(deletedCard));
+            });
+        });
+    });
+
+    it("can reset the deck to the default deck", () => {
+        const listOfDefaultCards = buildDefaultDeck().getListOfCards();
+        expect(siteWrap.state().deck).not.toEqual(expect.arrayContaining(listOfDefaultCards));
+
+        deckOps.resetDeck();
+
+        const listOfCardsAfterReset = siteWrap.state().deck.getListOfCards();
+        const currentCard = siteWrap.state().currentCard
+
+        expect(listOfCardsAfterReset).toEqual(expect.arrayContaining(listOfDefaultCards));
+        expect(listOfDefaultCards).toContainEqual(expect.objectContaining(currentCard));
     });
 
     it("resetting the deck notifies the user", () => {
-        throw Error("Test not yet implemented.");
+        const messageSpy = spyOn(message, "success");
+        expect(messageSpy).not.toHaveBeenCalled();
+        deckOps.resetDeck();
+        expect(messageSpy).toHaveBeenCalled();
+    });
+
+    function expectEachDeckOperationTo(testConditionsFor) {
+        // To test pre- and post- conditions of each deck operation.
+        // testConditionsFor(action) where action is called
+        // is the signature of the function parameter
+        it("appended a card", () => {
+            const additionalCard = new FlashCard("added", "new", "card", ["tag1", "tag2"]);
+
+            testConditionsFor(() => { deckOps.appendCard(additionalCard) });
+        });
+
+        it("edited a card", () => {
+            const DECK_KEY = 2;
+            const fakeValues = { front: "edited", back: "this", prompt: "card", tags: ["tag1", "tag2"] };
+
+            testConditionsFor(() => { deckOps.editCard(DECK_KEY, fakeValues) });
+        });
+
+        it("deleted a card", () => {
+            const DECK_KEY = 2;
+
+            testConditionsFor(() => { deckOps.deleteCard(DECK_KEY) });
+        });
+
+        it("deleted multiple cards", () => {
+            const DECK_KEYS = [3, 4, 5];
+
+            testConditionsFor(() => { deckOps.deleteCards(DECK_KEYS) });
+        });
+
+        it("reset the deck", () => {
+            testConditionsFor(deckOps.resetDeck);
+        });
+    }
+
+    describe("rebuild the deck upon having", () => {
+
+        const rebuildActiveSpy = jest.spyOn(Deck.prototype, "rebuildActive");
+        let menuKeys;
+
+        beforeEach(() => {
+            rebuildActiveSpy.mockClear();
+            menuKeys = siteWrap.instance().menuKeys;
+            siteWrap.setState({ selected: menuKeys.manage });
+        });
+
+        const rebuildDeck = (action) => {
+            expect(rebuildActiveSpy).not.toHaveBeenCalled();
+            action();
+            siteWrap.find(Menu).simulate('click', { key: menuKeys.review });
+            expect(rebuildActiveSpy).toHaveBeenCalledTimes(1);
+        };
+
+        expectEachDeckOperationTo(rebuildDeck);
+    });
+
+    describe("change active card upon having", () => {
+
+        const getNextCardSpy = jest.spyOn(Deck.prototype, "getNextCard");
+        let menuKeys;
+
+        beforeEach(() => {
+            getNextCardSpy.mockClear();
+            menuKeys = siteWrap.instance().menuKeys;
+            siteWrap.setState({ selected: menuKeys.manage });
+        });
+
+        const changeActiveCard = (action) => {
+            // Cleared after constructor so it's buildDefaultDeck call isn't recorded
+            expect(getNextCardSpy).not.toHaveBeenCalled();
+            action();
+            siteWrap.find(Menu).simulate('click', { key: menuKeys.review });
+            expect(getNextCardSpy).toHaveBeenCalledTimes(1);
+        };
+
+        expectEachDeckOperationTo(changeActiveCard);
+    });
+
+    describe("serialize the deck into local storage upon having", () => {
+
+        const saveDeck = (action) => {
+            /* Helper method to check serialization of deck. */
+            expect(localStorage.setItem).toHaveBeenCalledTimes(1);
+            expect(localStorage.setItem).toHaveBeenCalledWith("activeTags", expect.any(String));
+            action();
+            expect(localStorage.setItem).toHaveBeenCalledWith("savedDeck", expect.any(String));
+        }
+
+        expectEachDeckOperationTo(saveDeck);
     });
 });
 
