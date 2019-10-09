5629584f520b1708fa64673b085c9478a3a659d8
Tony Cheang <cheangytony@gmail.com>
2019-10-08 22:15:21 -0700

    Renamed dispatchTries to dispatchWithRedirect.

--- src/AuthenticationModal.js
+++ src/AuthenticationModal.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Modal, Button, Form, Input, Icon, message, Tooltip } from "antd";
 import ErrorBoundary from './ErrorBoundary';
 import './AuthenticationModal.css';
-import { dispatch, dispatchTries } from './Dispatch';
+import { dispatchWithRedirect } from './Dispatch';
 
 // Suppresses internal warnings in the dev console. (Since will warn after each key input).
 import Schema from 'async-validator';
@@ -77,7 +77,7 @@ class Login extends React.Component {
         const { onSuccess, onError, setLoading } = this.props;
         setLoading(true);
 
-        return dispatchTries("/auth/login", "POST", { username, password })
+        return dispatchWithRedirect("/auth/login", "POST", { username, password })
             .then(data => { console.log(data); return data })
             .then(data => { onSuccess(data) })
             .catch(err => onError(err))
@@ -176,7 +176,7 @@ class SignUp extends React.Component {
         const { onSuccess, onError, setLoading } = this.props;
         setLoading(true);
 
-        return dispatchTries("/auth/signup", "POST", { username, password, email })
+        return dispatchWithRedirect("/auth/signup", "POST", { username, password, email })
             .then(data => { console.log(data); return data })
             .then(data => onSuccess(data))
             .catch(err => onError(err))
